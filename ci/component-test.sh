12e87e8667672ad355de48b95b992984ebce97a1
Oliver Kirkegaard Svendsen <37912255+Nolle10@users.noreply.github.com>
2023-08-16 13:04:19 +0200

    Update component-test.sh

--- ci/component-test.sh
+++ ci/component-test.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
+
+# Convert docker_username to lowercase
+docker_username="${docker_username,,}"
+
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker_username=$DockerRepo docker-compose -f component-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
+docker-compose -f performance-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
